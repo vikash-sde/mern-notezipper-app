1c464445de132c89b7dd244e58d34002c4ae2aca
vikash_gupta <vikashg2512@gmail.com>
2022-02-09 16:13:42 +0530

    landing page setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
 
 function App() {
   return (
     <>
       <Header />
-      <main style={{ minHeight: "93vh" }}></main>
+      <main >
+        <LandingPage />
+      </main>
       <Footer />
     </>
   );
--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { Button, Container, Row } from "react-bootstrap";
+import "./landingpage.css";
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro_text">
+            <div>
+              <h1 className="title">Welcome to Note Zipper</h1>
+              <p className="subtitle">One Safe place for all your notes.</p>
+            </div>
+            <div className="button_container">
+              <a href="/login">
+                <Button size="lg" className="landing_button">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landing_button"
+                  variant="outline-primary"
+                >
+                  Signup
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default LandingPage;
