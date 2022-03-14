36ebd5256660600553acef795286735baafea5a1
vikash_gupta <vikashg2512@gmail.com>
2022-03-15 02:54:11 +0530

    Preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 
@@ -13,18 +14,27 @@ dotenv.config(app);
 connectDB();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// ---------------- deployment --------------------
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 console.log(process.env.PORT);
-
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
